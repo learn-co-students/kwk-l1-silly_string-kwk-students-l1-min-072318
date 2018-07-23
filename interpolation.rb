e40ad1aa04a1f8e08a0c5d33ac48ff3757abd1b3
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-23 20:52:31 +0000

    Done.

--- interpolation.rb
+++ interpolation.rb
@@ -1,20 +1,2 @@
-your_favorite_word = "truffles"
-puts "My favorite word is #{your_favorite_word}"
-favorite_word_in_all_caps = your_favorite_word.upcase
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
-phrase = "I LOVE Ryan Gosling"
-lowercased_phrase = phrase.upcase
-puts lowercased_phrase
-
-big_word = "wowmomupercalifragilisticexpialidocious"
-letter_count = big_word.length
-puts "There are #{letter_count} in #{big_word}"
-
-proper_sentence = "i love kode with klossy.".capitalize
-puts proper_sentence
-
-sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
-sentence = sentence.gsub(/[Aa]/,'A'=>'O','a'=>'o')
-puts sentence
 
--- silly_string.rb
+++ silly_string.rb
@@ -5,21 +5,26 @@ your_favorite_word = "truffles"
 
 puts "My favorite word is #{your_favorite_word}"
 
+
+
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word.upcase
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
+
+
+
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "I LOVE Ryan Gosling"
+phrase = "IM NOT SHOUTING"
 lowercased_phrase = phrase.downcase
 puts lowercased_phrase
 
 # Challenge 4: Count how many letters are in that big_word
 
-big_word = "wowsupersupercalifragilisticexpialidocious"
+big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word.length
 
@@ -27,15 +32,16 @@ puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word =  big_word + "wow" + "mom"
 
-new_letter_count = bigger_word.size
+new_letter_count = bigger_word.length
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
+
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i love kode with klossy.".capitalize
+proper_sentence = "i really like programming.".capitalize
 
 puts proper_sentence
 
